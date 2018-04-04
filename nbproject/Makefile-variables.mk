2680888d0dfbd695e1823216f7d231cad7b9378a
GarbiSebastian <garbyseba@gmail.com>
2018-04-04 10:26:02 -0300

    ignore requeridos

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=metnum_2018_1c_tp1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/metnum_2018_1c_tp1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=metnum20181ctp1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/metnum20181ctp1.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=metnum_2018_1c_tp1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/metnum_2018_1c_tp1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=metnum20181ctp1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/metnum20181ctp1.tar
+# prueba1 configuration
+CND_PLATFORM_prueba1=GNU-Linux
+CND_ARTIFACT_DIR_prueba1=dist/prueba1/GNU-Linux
+CND_ARTIFACT_NAME_prueba1=metnum_2018_1c_tp1
+CND_ARTIFACT_PATH_prueba1=dist/prueba1/GNU-Linux/metnum_2018_1c_tp1
+CND_PACKAGE_DIR_prueba1=dist/prueba1/GNU-Linux/package
+CND_PACKAGE_NAME_prueba1=metnum20181ctp1.tar
+CND_PACKAGE_PATH_prueba1=dist/prueba1/GNU-Linux/package/metnum20181ctp1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
